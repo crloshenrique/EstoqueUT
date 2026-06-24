0bd0e620378fa3068f700750d16b3842ee1d359d
Carloshenrique <carloshenriqueecr7@gmail.com>
2026-06-23 21:38:48 -0300

    SF

--- script.js
+++ script.js
@@ -1694,6 +1694,7 @@ function mostrarSecao(idSecao) {
             <section id="secao-inicio">
                 <h1>UnterTech</h1>
                 <p>Bem-vindo ao sistema de gestão. Selecione uma opção no menu lateral.</p>
+                <p>Version 13.1</p>
             </section>
             <section id="secao-registros" style="display:none;">
                 <h1>Registros de atividade</h1>
